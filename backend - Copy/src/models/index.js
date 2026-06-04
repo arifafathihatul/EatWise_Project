546df44764fd12977a1ee028fc1514064f5f5bdc
Your Name <you@example.com>
2026-06-04 15:41:29 +0700

    remove accidental backup folder and update gitignore

--- backend - Copy/src/models/index.js
+++
@@ -1,9 +0,0 @@
-import UserModel from './user.js';
-import FoodLogModel from './foodLog.js';
-import DailyTrackerModel from './dailyTracker.js';
-
-export {
-  UserModel,
-  FoodLogModel,
-  DailyTrackerModel
-};
